264c792c90b6c0f7c60ad4afc9a12c519d53f664
Andrew Mushel <almushel@protonmail.com>
2023-11-08 17:11:02 -0600

    Download and extract catalog file

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+import urllib.request
+import gzip
+# import csv
+
+BOOK_DIR = "books/"
+GB_HOST = "https://www.gutenberg.org"
+GB_CATALOG = "/cache/epub/feeds/pg_catalog.csv.gz"
+
+# Fetch Project Gutenberg catalog from GB_CATALOG 
+def get_catalog():
+	catalog_archive_path = f"{BOOK_DIR}{GB_CATALOG.split('/')[-1]}"
+
+	temp_file, headers = urllib.request.urlretrieve(f"{GB_HOST}{GB_CATALOG}")
+	temp_stream = open(temp_file, "rb")
+	temp_buf = temp_stream.read()
+
+	save_stream = open(catalog_archive_path, "wb")
+	save_stream.seek(0)
+	save_stream.write(temp_buf)	
+
+	temp_stream.close()
+	save_stream.close()
+
+	save_stream = open(catalog_archive_path[:-3], "wb")
+	gz_file = gzip.open(catalog_archive_path)
+	gz_buffer = gz_file.read()
+	save_stream.write(gz_buffer)
+
+	save_stream.close()
+	gz_file.close()
+
 def count_words(words):
 	return len(words.split())
 
@@ -13,26 +44,27 @@ def count_letters(words):
 	
 	return result
 
-def main():
-	book_dir = "books/frankenstein.txt"
-	contents = ""
-	
-	with open(book_dir) as f:
-		contents = f.read()
-	
-	print(f"--- Begin report of {book_dir} ---\n")
-	print(f"{count_words(contents)} words found in the document\n")
+if __name__ == "__main__":
+	if False:	
+		get_catalog()
+	else:
+		book_dir = f"{BOOK_DIR}frankenstein.txt"
+		contents = ""
+		
+		with open(book_dir) as f:
+			contents = f.read()
+		
+		print(f"--- Begin report of {book_dir} ---\n")
+		print(f"{count_words(contents)} words found in the document\n")
 
-	letters = count_letters(contents)
-	def get_count(e):
-		return letters[e]
+		letters = count_letters(contents)
+		def get_count(e):
+			return letters[e]
 
-	letter_list = list(letters)
-	letter_list.sort(reverse=True, key=get_count)
+		letter_list = list(letters)
+		letter_list.sort(reverse=True, key=get_count)
 
-	for l in letter_list:
-		print(f"The {l} character was found {letters[l]} times")
-	
-	print("\n--- End report ---")
-
-main()
+		for l in letter_list:
+			print(f"The {l} character was found {letters[l]} times")
+		
+		print("\n--- End report ---")
