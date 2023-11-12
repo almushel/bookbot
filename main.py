683aa48ff95b3fb6f01cea65df8ef16833e582bb
Andrew Mushel <almushel@protonmail.com>
2023-11-12 12:27:16 -0600

    Support multiple ebook formats

--- main.py
+++ main.py
@@ -6,13 +6,36 @@ import urllib.request
 import gzip
 import csv
 
-BOOK_DIR = "books/"
+DEFAULT_BOOK_DIR = "books/"
 GB_HOST = "https://www.gutenberg.org"
-GB_CATALOG = "/cache/epub/feeds/pg_catalog.csv.gz"
-ARCHIVE_PATH = f"{BOOK_DIR}{GB_CATALOG.split('/')[-1]}"
+GB_CACHE_PATH = "/cache/epub/"
+GB_CATALOG_PATH = GB_CACHE_PATH+"feeds/pg_catalog.csv.gz"
+
+FILE_EXTENSIONS = {
+	"html"					: "-images.html",
+	"epub3"					: "-images-3.epub",
+	"epub"					: "-images.epub",
+	"epub-no-images"		: ".epub",
+	"kindle"				: "-images-kf8.mobi",
+	"kindle-old"			: "-images.mobi",
+	"kindle-old-no-images"	: ".mobi",
+	"txt"					: ".txt",
+	"txt-utf8"				: ".txt.utf8"
+}
+
+current_book_dir = DEFAULT_BOOK_DIR
+
+def get_local_catalog_path():
+	return current_book_dir+"catalog.csv.gz"
 
 def catalog_exists():
-	return os.path.isfile(ARCHIVE_PATH)
+	return os.path.isfile( get_local_catalog_path() )
+
+def format_file_name(title_number, title):
+	if title is None:
+		return f"{title_number}"
+	else:
+		return f"{title_number} - {title}"
 
 def parse_date_header(date):
 	# Mon, 00 Jan 2023 00:00:00 GMT
@@ -41,13 +64,13 @@ def parse_date_header(date):
 	
 def check_for_catalog_updates():
 	try:
-		response = urllib.request.urlopen(f"{GB_HOST}{GB_CATALOG}")
+		response = urllib.request.urlopen(GB_HOST+GB_CATALOG_PATH)
 	except urllib.error.URLError as e:
 		print(e)	
 		return False, None
 	else:
 		if catalog_exists():
-			local_modified = time.gmtime( os.path.getmtime(ARCHIVE_PATH) )
+			local_modified = time.gmtime( os.path.getmtime( get_local_catalog_path() ) )
 			remote_modified = parse_date_header( response.getheader("last-modified") )
 
 			for i in range(len(remote_modified)):
@@ -60,17 +83,17 @@ def check_for_catalog_updates():
 def download_catalog(response=None):
 	try:
 		if response is None:
-			response = urllib.request.urlopen(f"{GB_HOST}{GB_CATALOG}")
+			response = urllib.request.urlopen(f"{GB_HOST}{GB_CATALOG_PATH}")
 	except urllib.error.URLError as e:
 		print(e)	
 		return False
 	else:
-		if not os.path.isdir(BOOK_DIR): os.makedirs(BOOK_DIR)
+		if not os.path.isdir(DEFAULT_BOOK_DIR): os.makedirs(DEFAULT_BOOK_DIR)
 
 		temp_buf = response.read()
 		response.close()
 
-		with open(ARCHIVE_PATH, "wb") as save_stream:
+		with open(get_local_catalog_path(), "wb") as save_stream:
 			save_stream.seek(0)
 			save_stream.write(temp_buf)
 
@@ -78,10 +101,12 @@ def download_catalog(response=None):
 
 def search_catalog(keywords, fields=["Title"], language="en"):
 	result = []
-	with gzip.open(ARCHIVE_PATH, "rt") as csvfile:
+	with gzip.open(get_local_catalog_path(), "rt") as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
 			if row["Language"] != language: continue
+			if row["Type"] == "Sound": continue
+
 			for key in row:
 				if key not in fields: continue
 				for word in keywords:
@@ -89,58 +114,24 @@ def search_catalog(keywords, fields=["Title"], language="en"):
 						result.append(row)
 	return result
 
-def download_title(title_number, title=None):
-	if title is None:
-		file_name = f"{title_number}"
-	else:
-		file_name = f"{title_number} - {title}"
-	url = f"{GB_HOST}/files/{title_number}/{title_number}-0.txt"
+def download_title(title_number, title=None, format = "txt"):
+	file_name = format_file_name(title_number, title)
+	url = GB_HOST+GB_CACHE_PATH+f"/{title_number}/pg{title_number}"+FILE_EXTENSIONS[format]
 
 	try:
 		response = urllib.request.urlopen(url)
 	except urllib.error.URLError as e:
 		print(e)
+		return False
 	else:
+		file_path = current_book_dir+file_name+FILE_EXTENSIONS[format]
 		buf = response.read() 
-		with open(f"{BOOK_DIR}{file_name}.txt", "wb") as save_file:
+		with open(file_path, "wb") as save_file:
 			save_file.seek(0)
 			save_file.write(buf) 
+		response.close()
 
-def count_words(words):
-	return len(words.split())
-
-def count_letters(words):
-	result = {}
-	for l in words.lower():
-		if not l.isalpha(): continue
-
-		if (l in result):
-			result[l] += 1
-		else:
-			result[l] = 0
-	
-	return result
-
-def print_report(book_path):
-	contents = ""
-	
-	with open(book_path) as f:
-		contents = f.read()
-	
-	print(f"--- Begin report of {book_path} ---\n")
-	print(f"{count_words(contents)} words found in the document\n")
-
-	letters = count_letters(contents)
-	def get_count(e):
-		return letters[e]
-
-	letter_list = list(letters)
-	letter_list.sort(reverse=True, key=get_count)
-
-	for l in letter_list:
-		print(f"The {l} character was found {letters[l]} times")
-	
-	print("\n--- End report ---")
+	return True
 
 if __name__ == "__main__":
 	keywords = ["frankenstein"]
@@ -157,16 +148,21 @@ if __name__ == "__main__":
 	if catalog_exists():
 		print(f"Searching for keywords {keywords}...") 
 		search_results = search_catalog(keywords)
-		for result in search_results:
-			book_path = f"{BOOK_DIR}{result['Text#']}-{result['Title']}.txt"
-			if os.path.isfile(book_path):
-				print(f"Local TXT file for {result['Text#']}: {result['Title']}" )
-			else:
-				print(f"Attempting to download .txt format for {result['Text#']}: {result['Title']}")
-				download_title(result["Text#"], result["Title"])
+		formats = ["txt", "epub"]
 
-			if os.path.isfile(book_path):
-				print(f"Downloaded {result['Text#']}: {result['Title']}")
-#				print_report(book_path)
-	else:
+		for result in search_results:
+			for format in formats:
+				book_path = current_book_dir+format_file_name(result["Text#"], result["Title"])+FILE_EXTENSIONS[format]
+				if os.path.isfile(book_path):
+					print(f"Local copy exists for {format} for {result['Text#']}: {result['Title']}.")
+					continue
+
+				print(f"Attempting to download {format} for {result['Text#']}: {result['Title']}.")
+
+				success = download_title(result["Text#"], result["Title"], format)
+
+				if success:
+					print(f"Downloaded {result['Text#']}: {result['Title']} ({format}).")
+
+	else: # not catalog_exists()
 		print("Failed to open catalog")
