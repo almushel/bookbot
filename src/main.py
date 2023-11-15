23ff48588241a292f91c29cc7529cc32ec78d309
Andrew Mushel <almushel@protonmail.com>
2023-11-15 17:28:41 -0600

    Support for separate search and download modes

--- src/main.py
+++ src/main.py
@@ -37,17 +37,21 @@ if __name__ == "__main__":
 		description="A command-line utility for downloading public domain ebooks from Project Gutenberg"
 	)
 	
-	# TO-DO: keywords behavior differs based on program mode:
-	# 	"search" & "both": search keywords
-	# 	"download": book download title#s
-	parser.add_argument("keywords", type=comma_separated_list)
-	parser.add_argument("-m", "--mode", type=str, choices=["search", "download", "both"], default="both")
-	parser.add_argument("-f", "--formats", type=comma_separated_list, default=["txt"])
-	parser.add_argument("-s", "--fields", type=comma_separated_list, default=["Title"])
+	parser.add_argument("keywords", type=comma_separated_list, help="Search keywords. In (-d)ownload mode, list of ebook text #s")
+	parser.add_argument("-f", "--formats", type=comma_separated_list, default=["txt"], help="ebook formats to download")
+	parser.add_argument("-f2", "--fields", type=comma_separated_list, default=["Title"], help="Metadata fields to search for keywords")
+#	parser.add_argument("-o", "--out", type=str, help="Output directory for downloaded files") 
+#	parser.add_argument("-r", "--report", type=str, help="Print metadata report for search results.")
+
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument("-d", "--download", action="store_true", help="Download ebooks, given list of Project Gutenbook book text #s")
+	group.add_argument("-s", "--search", action="store_true", help="Search catalog and print as list of text #s")
 	
 	args = parser.parse_args()
 	if not validate_args(args):
 		exit()
+
+#	if args.out: greb.book_dir = args.out
 
 	print("Checking for catalog updates...")
 	update, response = greb.check_for_catalog_updates()	
@@ -58,10 +62,24 @@ if __name__ == "__main__":
 	else:
 		print("No updates found.")	
 
-	if greb.catalog_exists():
-		print(f"Searching for keywords {args.keywords} in fields {args.fields}...") 
-		search_results = greb.search_catalog(args.keywords, args.fields)
+	search_results = []
+	if not args.download:
+		if greb.catalog_exists():
+			print(f"Searching for keywords {args.keywords} in fields {args.fields}...") 
+			search_results = greb.search_catalog(args.keywords, args.fields)
+		else: # not catalog_exists()
+			print("Failed to open catalog")
 
+	else: # Download-only mode
+		for word in args.keywords:
+			if word.isnumeric():
+				result = greb.search_catalog([word], ["Text#"])
+				if len(result):	search_results.append( result[0] )	
+				else:			print(f"Text number {word} not found.")
+			else:
+				print(f"Invalid Text#: {word}")	
+
+	if not args.search:
 		for result in search_results:
 			for format in args.formats:
 				book_path = greb.book_dir+greb.format_file_name(result["Text#"], result["Title"], format)
@@ -75,6 +93,9 @@ if __name__ == "__main__":
 
 				if success:
 					print(f"Downloaded {result['Text#']}: {result['Title']} ({format}).")
-
-	else: # not catalog_exists()
-		print("Failed to open catalog")
+	else: # search-only mode
+		output = ""
+		for result in search_results:
+			output += result["Text#"]+","
+		output = output[:-1]
+		print(output)
--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #! bin/sh
 
-python src/main.py Frankenstein --formats txt,epub,trash,garbage 
+echo Testing search-only
+python src/main.py Frankenstein -s -f txt,epub,ignore
+
+echo ========================
+echo Testing download-only
+python src/main.py 84,41445,42324 -d --formats txt,epub
+
+echo ========================
+echo Testing search and download
+python src/main.py Frankenstein --formats txt,epub,trash,garbage
